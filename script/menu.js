8f2fc8493372cbe0da64598d541d104f2693402a
helloredreyes <hello.silentlymine@gmail.com>
2024-06-14 20:15:39 +0700

    sync

--- script/menu.js
+++ script/menu.js
@@ -10,12 +10,15 @@ function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+      tabcontent[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// Get the element with id="defaultOpen" and click on it to display the default tab content
+document.getElementById("defaultOpen").click();
