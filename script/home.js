7d7e635393f6140b1e84b4d33565fd46948edc17
helloredreyes <hello.silentlymine@gmail.com>
2024-06-14 18:06:30 +0700

    sync

--- script/home.js
+++ script/home.js
@@ -40,26 +40,3 @@ function currentSlide(n) {
 
 // Display the first slide when the page loads
 showSlides(slideIndex);
-
-// DELAY ANIMATION
-
-document.addEventListener('DOMContentLoaded', () => {
-    const observerOptions = {
-        root: null, // Use the viewport as the root
-        rootMargin: '0px',
-        threshold: 0.1 // Trigger when 10% of the element is visible
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate');
-                observer.unobserve(entry.target); // Stop observing once animation is triggered
-            }
-        });
-    }, observerOptions);
-
-    document.querySelectorAll('.hidden').forEach(element => {
-        observer.observe(element);
-    });
-});
